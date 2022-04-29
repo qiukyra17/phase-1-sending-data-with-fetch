1f5642630b619e98d94667ab3d28b6ad92c11726
kyra qiu <kyraqiu@gmail.com>
2022-04-28 21:17:51 -0400

    commit tree 19868228fae2a66558963b31ce776a73d2f0be4a

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+const userData = {
+    name: "Steve",
+    email: "steve@steve.com"
+};
+
+const configurationObj = {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body:JSON.stringify(userData),
+};
+
+function submitData(name, email) {
+    return fetch ('http://localhost:3000/users', configurationObj)
+    .then (res => res.json())
+    .then (function (newID){
+        document.querySelector('body');
+        document.body.innerHTML = newID.id;
+    })
+    .catch (function (err){
+        alert ('ERROR ERROR');
+       document.body.innerHTML = err.message;
+    });
+}
+
+
